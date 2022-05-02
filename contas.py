9c7046e1e7bd2f2b4813906558282d34db16ccb7
rogeriodelphi <rogeriodelphi@gmail.com>
2022-05-01 21:52:48 -0300

    Primeiro commit

---
+++ contas.py
@@ -0,0 +1,10 @@
+numero = 123
+titular = "Rogerio"
+saldo = 55.0
+limite = 1000.0
+
+conta = {'numero':123, 'titular':'Rogerio', 'saldo':55.0, 'limite':1000.0}
+print(conta['numero'])
+
+conta2 = {'numero':124, 'titular':'Sheilla', 'saldo':100.0, 'limite':4000.0}
+
