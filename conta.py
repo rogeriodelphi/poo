b2c1558190d8beb520cb117bf4ec17b4e76e8abf
rogeriodelphi <rogeriodelphi@gmail.com>
2022-05-02 01:06:52 -0300

    Usando métodos

--- conta.py
+++ conta.py
@@ -1,10 +1,21 @@
 class Conta:
-    def __init__(self, numero, titular, saldo, limite): # função construtora
+    def __init__(self, numero, titular, saldo, limite = 1000.0): # função construtora
         print('Construindo objeto ...{}'.format(self))
         self.numero = numero
-        self.titulo = titular
+        self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print('O titular {} possui o saldo de R$ {} em sua conta de número {} '.format(self.titular, self.saldo, self.numero))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+
 
 
         # obs: self é a referência que sabe encontar aquele objeto que está sendo criado no módulo.
