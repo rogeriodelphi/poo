b91cad8009d47ac1d4012f8295defa0d071f9e11
rogeriodelphi <rogeriodelphi@gmail.com>
2022-05-03 01:05:56 -0300

    Deixando os atributos como privados

--- conta.py
+++ conta.py
@@ -1,22 +1,22 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite = 1000.0): # função construtora
         print('Construindo objeto ...{}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print('O titular {} possui o saldo de R$ {} em sua conta de número {} '.format(self.titular, self.saldo, self.numero))
+        print('O titular {} possui o saldo de R$ {} em sua conta de número {} '.format(self.__titular, self.__saldo, self.__numero))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
 
 
 
         # obs: self é a referência que sabe encontar aquele objeto que está sendo criado no módulo.
-
+#       # __saldo deixa o atributo privado - encapsula o acesso ao atributo
